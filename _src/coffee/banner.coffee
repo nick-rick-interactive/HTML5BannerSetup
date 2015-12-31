046ab6ba1d3c91fb05290d53c13b056cdfeb9457
Nick Rickenbach <nick.rick.interactive@gmail.com>
2015-12-30 17:32:09 -0800

    added batter file generating with size and prefix variables placed into files

--- _src/coffee/banner.coffee
+++ _src/coffee/banner.coffee
@@ -1,55 +1,200 @@
 class Banner
 
-  constructor: (_width=0, _height=0, _loop=3) ->
+  constructor: (_width=0, _height=0, _type="in-page", _loop=3) ->
     @width = _width
     @height = _height
+    @type = _type
     @loop = _loop
     @curLoop = 1
 
+    /// STORE ELEMENTS THAT WILL BE ANIMATED ///
+
     @els = $("#main").find(".ani-elem");
 
-    $("overlay").bind("click",@.exit)
+    /// ADD UI LISTENERS FOR EACH TYPE ///
+
+    switch @type
+
+      when "in-page"
+
+        $("#overlay").bind("click",@exit);
+        $("#cta").bind("click",@exit);
+
+      when "expanding"
+
+        $("#overlay").bind("click",@exit);
+        $("#cta").bind("click",@toggleExpand);
+
+        $("#overlay-exp").bind("click",@exit);
+        $("#cta-exp").bind("click",@exit);
+        $("#close-exp").bind("click",@toggleExpand);
+
+      when "floating"
+
+        $("#overlay").bind("click",@exit);
+        $("#cta").bind("click",@toggleExpand);
+
+        $("#overlay-float").bind("click",@exit);
+        $("#cta-float").bind("click",@exit);
+        $("#close-float").bind("click",@close);
+
+    /// ADD DOUBLECLICK LISTENERS ///
+
+    if @type != "in-page"
+
+      Enabler.addEventListener(studio.events.StudioEvent.EXPAND_START, @expandStartHandler);
+      Enabler.addEventListener(studio.events.StudioEvent.EXPAND_FINISH, @expandFinishHandler);
+      Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_START, @collapseStartHandler);
+      Enabler.addEventListener(studio.events.StudioEvent.COLLAPSE_FINISH, @collapseFinishHandler);
 
     if (Enabler.isInitialized())
+
       Banner.enablerInitHandler();
+
     else
+
       Enabler.addEventListener(studio.events.StudioEvent.INIT, @enablerInitHandler);
 
   enablerInitHandler: (e) =>
-    console.log e
+
+    /// CHECK PAGE IS LOADED ///
+
     if (Enabler.isPageLoaded())
+
       Banner.checkVisible();
+
     else
+
       Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, @checkVisible);
 
   checkVisible: (e) =>
+
+    /// CHECK PAGE IS VISIBLE ///
+
     if (Enabler.isVisible())
+
       Banner.init();
+
     else
+
       Enabler.addEventListener(studio.events.StudioEvent.VISIBLE, @init);
 
+
+
+  /// OVERWRITE FUNCTION (REQUIRED) ///
+
   init: (e) =>
+
     alert "you must add an init function to the extended coffeescript class"
 
+  /// OVERWRITE FUNCTION (REQUIRED) ///
+
+  expandInit: () =>
+
+    alert "you must add an expand init function to the extended coffeescript class"
+
+  /// OVERWRITE FUNCTION (REQUIRED) ///
+
+  closeInit: () =>
+
+    alert "you must add an close init function to the extended coffeescript class"
+
+
+  /// OVERWRITE FUNCTION ///
+
+  expandStartHandler: (e) =>
+
+    Enabler.finishExpand();
+
+
+  /// OVERWRITE FUNCTION ///
+
+  expandFinishHandler: (e) =>
+
+    @isExpanded = true;
+
+    @expandInit();
+
+
+  /// OVERWRITE FUNCTION ///
+
+  collapseStartHandler: (e) =>
+
+    Enabler.finishCollapse();
+
+
+  /// OVERWRITE FUNCTION ///
+
+  collapseFinishHandler: (e) =>
+
+    @isCollapsed = true;
+
+
+
+  /// BASE CLASS FUNCTIONS ///
+
   end: (_func, _dur) =>
+
     if @curLoop == @loop
+
       clearInterval(@switchInt);
+
     else
+
       @curLoop++;
       @replay(_func, _dur);
 
   exit: (e) =>
+
     if (@switchInt)
+
       clearInterval(@switchInt);
 
-    Enabler.exit(_exit="exit")
+    Enabler.exit("exit")
 
   replay: (_func, _dur) =>
+
     $(el)[0].removeAttribute("style") for el in @els
+
     @switchFunc(_func, _dur)
 
+
+  /// UTILITY CLASS FUNCTIONS ///
+
+  toggleExpand: =>
+
+    @isExpanded = if @isExpanded then Enabler.requestCollapse() else Enabler.requestExpand();
+
+  close: =>
+
+    Enabler.reportManualClose();
+    Enabler.close();
+
+    @closeInit();
+
+  autoClose: =>
+
+    if(@autoTimer)
+
+      clearInterval(@autoTimer);
+
+    Enabler.close();
+
+    @closeInit();
+
+
   switchFunc: (_func, _dur=0) =>
+
     if (@switchInt)
+
       clearInterval(@switchInt);
 
     @switchInt = setInterval(_func,_dur*1000);
+
+  addCounter: (_el, _text) =>
+
+    $(_el).click( () =>
+
+      Enabler.counter(_text)
+
+    )
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var path = require('path');
 
 var gulp = require('gulp');
-var data = require('gulp-data');
 
 //GET ARGUMENTS FOR BANNER SIZE
 var argv = require('yargs').argv;
@@ -16,6 +15,7 @@ var plugins = require('gulp-load-plugins')();
 var runSequence = require('run-sequence');
 
 var rename = require('gulp-rename');
+var replace = require('gulp-replace-task');
 var concat = require('gulp-concat');
 var del = require('del');
 
@@ -34,7 +34,11 @@ var zip = require('gulp-zip');
 var pkg = require('./package.json');
 
 //DEFAULT SIZE TO 728x90
-var filename = (argv.size) ? argv.size : "728x90";
+var vals = ["Banner",728,90];
+var filename = (argv.banner) ? argv.banner : "Banner_728x90";
+var newVals = ["Banner",728,90];
+var newFilename = (argv.newBanner) ? argv.newBanner : "Banner_728x90";
+var bannerType = "in-page";
 
 var dirs = pkg['configs'].directories;
 
@@ -157,6 +161,11 @@ gulp.task('serve', function () {
 
 // MAIN FUNCTIONS
 
+/**
+ * Compile and shrink a banner
+ * --size = (banner size to compile) REQUIRED
+ **/
+
 gulp.task('build', function (done) {
     runSequence(
         'clean',
@@ -171,6 +180,32 @@ gulp.task('build', function (done) {
 
 
 // COPY BANNER
+
+/**
+ * Copy one banner size to a new size
+ *
+ * --banner (REQUIRED) = Banner to copy. Variables separated by *
+ *      Prefix          REQUIRED
+ *      Width           REQUIRED
+ *      Height          REQUIRED
+ *      Exp-Width       REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *      Exp-Height      REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *
+ *      i.e. (--banner=Banner*728*90)
+ *
+ * --newBanner (REQUIRED) = New Banner. Variables separated by *
+ *      Prefix          REQUIRED
+ *      Width           REQUIRED
+ *      Height          REQUIRED
+ *      Exp-Width       REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *      Exp-Height      REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *
+ *      i.e. (--newBanner=Banner*728*90*728*200*0*0)
+ *
+ * --type (REQUIRED) = in-page, expandable or floating [DEFAULT = in-page]
+ *
+ **/
+
 gulp.task('copy-banner', function (done) {
     runSequence(
         "cb-move-files",
@@ -180,29 +215,66 @@ gulp.task('copy-banner', function (done) {
 });
 
 gulp.task('cb-move-files', function () {
+
+    vals = (argv.banner) ? argv.banner.split("*") : ["Banner","728","90","728","200"];
+    newVals = (argv.newBanner) ? argv.newBanner.split("*") : ["Banner","300","250","300","600"];
+
+    newVals[3] = newVals[3] ? newVals[3] : newVals[1]; // FOR EXPANDING AND FLOATING UNITS
+    newVals[4] = newVals[4] ? newVals[4] : newVals[2]+100; // FOR EXPANDING AND FLOATING UNITS
+    newVals[5] = newVals[5] ? newVals[5] : "0"; // FOR EXPANDING UNITS
+    newVals[6] = newVals[6] ? newVals[6] : "0"; // FOR EXPANDING UNITS
+
+    bannerType = (argv.type) ? argv.type : bannerType;
+
+    filename = (bannerType=="in-page") ? vals[0]+"_"+vals[1]+"x"+vals[2] : vals[0]+"_"+vals[1]+"x"+vals[2]+"_exp_"+vals[3]+"x"+vals[4];
+    newFilename = (bannerType=="in-page") ?  newVals[0]+"_"+newVals[1]+"x"+newVals[2] : newVals[0]+"_"+newVals[1]+"x"+newVals[2]+"_exp_"+newVals[3]+"x"+newVals[4];
+
     return gulp.src([
-        filename + "/**/*",
-        "!" + filename + "/dist/*"
-    ])
-        .pipe(gulp.dest(argv.newSize))
+            filename + "/**/*",
+            "!" + filename + "/dist/*"
+        ])
+        .pipe(gulp.dest(newFilename))
 });
 
 gulp.task('cb-rename', function () {
     return gulp.src(argv.newSize+"/_src/**/*")
-        .pipe(rename(function(path){
-            path.basename = argv.newSize
+        .pipe(replace({
+            patterns: [
+                {match: /filename/g,replacement: newFilename}
+            ]
         }))
-        .pipe(gulp.dest(argv.newSize+"/_src/"))
+        .pipe(rename(function(path){
+            path.basename = newFilename
+        }))
+        .pipe(gulp.dest(newFilename+"/_src/"))
 });
 
 gulp.task('cb-remove-temp', function () {
     return del([
-            argv.newSize+"/_src/"+filename+".*"
+        newFilename+"/_src/"+filename+".*"
     ])
 });
 
 
 // NEW BANNER
+
+/**
+ * Create a new banner
+ *
+ * --banner (REQUIRED) = Variables separated by *  [DEFAULT = Banner*728*90]
+ *      Prefix          REQUIRED
+ *      Width           REQUIRED
+ *      Height          REQUIRED
+ *      Exp-Width       REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *      Exp-Height      REQUIRED FOR EXPANDING AND FLOATING UNITS
+ *      Offset-X        OPTIONAL FOR EXPANDING UNITS
+ *      Offset-Y        OPTIONAL FOR EXPANDING UNITS
+ *
+ *      i.e. (--banner=Banner*728*90*728*200*0*0)
+ *
+ * --type (REQUIRED) = in-page, expandable or floating [DEFAULT = in-page]
+ *
+ **/
 
 gulp.task('new-banner', function (done) {
     runSequence(
@@ -213,12 +285,34 @@ gulp.task('new-banner', function (done) {
 });
 
 gulp.task('nb-move-files', function () {
-    return gulp.src("_template/**/*")
+
+    vals = (argv.banner) ? argv.banner.split("*") : ["Banner","728","90","728","200","0","0"];
+    vals[3] = vals[3] ? vals[3] : vals[1]; // FOR EXPANDING AND FLOATING UNITS
+    vals[4] = vals[4] ? vals[4] : vals[2]+100; // FOR EXPANDING AND FLOATING UNITS
+    vals[5] = vals[5] ? vals[5] : "0"; // FOR EXPANDING UNITS
+    vals[6] = vals[6] ? vals[6] : "0"; // FOR EXPANDING UNITS
+
+    bannerType = (argv.type) ? argv.type : bannerType;
+
+    filename = (bannerType=="in-page") ? vals[0]+"_"+vals[1]+"x"+vals[2] : vals[0]+"_"+vals[1]+"x"+vals[2]+"_exp_"+vals[3]+"x"+vals[4];
+
+    return gulp.src("_template/"+bannerType+"/**/*")
         .pipe(gulp.dest(filename))
 });
 
 gulp.task('nb-rename', function () {
     return gulp.src(filename+"/_src/**/*")
+        .pipe(replace({
+            patterns: [
+                {match: /_PREFIX_/g,replacement: vals[0]},
+                {match: /_WIDTH_/g,replacement: vals[1]},
+                {match: /_HEIGHT_/g,replacement: vals[2]},
+                {match: /_EXP_W_/g,replacement: vals[3]},
+                {match: /_EXP_H_/g,replacement: vals[4]},
+                {match: /_OFFSET-X_/g,replacement: vals[5]},
+                {match: /_OFFSET-Y_/g,replacement: vals[6]}
+            ]
+        }))
         .pipe(rename(function(path){
             path.basename = filename
         }))
